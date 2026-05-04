d2e207b55c279973fcf611de5e13f16269b14c74
Ktech312 <eck1679@gmail.com>
2026-05-04 10:22:05 -0700

    design: load vault hardening stylesheet

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata, Viewport } from "next";
 
 import "./globals.css";
 import "./vltd-design.css";
+import "./vault-pass.css";
 import TopNav from "@/components/TopNav";
 import Providers from "@/components/Providers";
 import { ThemeBoot } from "@/components/ThemeBoot";
