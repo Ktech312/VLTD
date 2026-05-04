8a2a6f7419deaec61b1f96a985d58e41e364cd58
Ktech312 <eck1679@gmail.com>
2026-05-04 12:19:21 -0700

    Design pass 6: Museum visual alignment
    Add Museum page visual alignment stylesheet and load it globally.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata, Viewport } from "next";
 import "./globals.css";
 import "./vltd-design.css";
 import "./vault-pass.css";
+import "./museum-pass.css";
 import TopNav from "@/components/TopNav";
 import Providers from "@/components/Providers";
 import { ThemeBoot } from "@/components/ThemeBoot";
